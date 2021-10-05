a081b80b409e9fa536768c0bc83c09f0e09f948f
rinabang <91496352+rinabang@users.noreply.github.com>
2021-10-05 22:48:03 +0300

    Delete Desktop/doc directory

--- Desktop/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"lab1","c":"Interface1","l":"Interface1()","url":"%3Cinit%3E()"},{"p":"lab1","c":"Interface1","l":"main(String[])","url":"main(java.lang.String[])"}]
--- Desktop/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"lab1","l":"Interface1"}]
